efa920a7aa74df6cb875429a555479371d5c273d
logan-gosch <logan.gosch.temp@gmail.com>
2026-04-22 06:26:58 -0500

    feat: moved functionality of events

--- src/parkevents/entities/Concert.java
+++ src/parkevents/entities/Concert.java
@@ -1,11 +1,15 @@
 package parkevents.entities;
 
 import java.time.LocalDate;
+import parkevents.EventType;
 
-import static parkevents.EventType.CONCERT;
-
-public class Concert extends Event {
+public class Concert extends AbstractEvent {
+    private String artistName;
     public Concert(String title, String description, LocalDate date) {
-        super(title, CONCERT, description, date);
+        super(title, description, date, EventType.CONCERT);
+    }
+
+    public String getArtistName() {
+        return artistName;
     }
 }
